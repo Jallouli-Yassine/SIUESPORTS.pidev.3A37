25b54e83137f2ab0b8987c166c2d3f353d4e8d7a
Jallouli-Yassine <jungloul1@gmail.com>
2023-02-12 20:42:12 +0100

    all courses and coursesDetails front

--- src/Controller/CoachingController.php
+++ src/Controller/CoachingController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CoachingController extends AbstractController
 {
-    #[Route('/coaching/allCourses', name: 'app_coache')]
+    #[Route('/coaching/allCourses', name: 'app_coaching')]
     public function index(): Response
     {
         return $this->render('coaching/allCoaching.html.twig', [
             'controller_name' => 'CoachingController',
         ]);
     }
+
+    #[Route('/coaching/oneCourse', name: 'oneCourse')]
+    public function oneCourse(): Response
+    {
+        return $this->render('coaching/CourseDetails.html.twig', [
+            'controller_name' => 'CoachingController',
+        ]);
+    }
+
+
 }
