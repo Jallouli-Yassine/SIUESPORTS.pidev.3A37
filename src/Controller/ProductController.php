ad65cb3ff53683ed592b619dfe8ebe19c8bdd00c
kamelfridhi <80178238+kamelfridhi@users.noreply.github.com>
2023-02-13 16:17:03 +0100

    update

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
     public function index(): Response
     {
+        $produit =$this->getDoctrine()->getRepository(Produit::class)->findAll();
+        //dd($produit);
         return $this->render('product/store.html.twig', [
-            'controller_name' => 'ProductController',
+            'produit' => $produit
+        ]);
+    }
+
+    /*#[Route('/consulteproduct/{id}', name: 'afficheproduit')]
+    public function update(Request $request,int $id): Response
+    {
+        $produit = $doctrine->getRepository(Produit::class)->find($id);
+
+        return $this->renderForm('product/produit.html.twig',
+            [
+                'p'=>$produit
+            ]);
+    }*/
+    #[Route('/consulteproduct/{id}', name: 'afficheproduit')]
+    public function oneProduct(int $id, ProduitRepository $produitRepository)
+    {
+        $produit = $produitRepository->find($id);
+
+        return $this->render('product/produit.html.twig', [
+            'p'=>$produit
+        ]);
+    }
+    #[Route('/consultePanier', name: 'affichePanier')]
+    public function Panier( ProduitRepository $produitRepository)
+    {
+
+
+        return $this->render('product/panier.html.twig', [
+
         ]);
     }
 }
